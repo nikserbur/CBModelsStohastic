9a95cd0385bc297cc5b8c7f1b294dc4d11157f4c
nikserbur <quasiperelman@gmail.com>
2025-07-08 18:13:01 +0300

    add data

--- 1.R/9.libraries.r
+++ 1.R/9.libraries.r
@@ -1,5 +1,5 @@
 `%!in%` <- Negate(`%in%`)
-libs <- c("qs","data.table","plotly","tidyverse","stats4","fitdistrplus","stabledist","scoringRules")
+libs <- c("qs","data.table","plotly","tidyverse","stats4","fitdistrplus","stabledist","scoringRules","readxl","alphastable")
 
 new.packages <- libs[!(libs %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
--- 1.R/Main.R
+++ 1.R/Main.R
@@ -7,28 +7,97 @@ source("1.R/8.SourceAll.R")
 
 # 1. Данные (загрузка, очистка, создание лагов/гепов) ####
 # исходные данные тут https://www.cbr.ru/dkp/statistics/
-#
-# orig_data_
-#
-#
-#
-#   setorder(date) %>%
-#   # лаги
-#   .[, `:=`(
-#     gdp_gap_lag1      = shift(gdp_gap, 1),
-#     cpi_infl_lag1     = shift(cpi_inflation, 1)
-#   )] %>%
-#   # геп ВВП (LOESS-тренд)
-#   .[, `:=`(
-#     gdp_trend = loess(gdp_level ~ as.numeric(date), span = 0.3)$fitted,
-#     gdp_gap2  = (gdp_level - gdp_trend) / gdp_trend * 100
-#   )]
+
+# Данные и инфляции
+inflation <- read_xlsx("3.Data/Инфляция и ключевая ставка Банка России_F01_12_2013_T18_06_2025.xlsx") %>%
+  as.data.table() %>%
+  .[,`Дата` := as.Date(paste0("01.", `Дата`), format = "%d.%m.%Y")] %>%
+  na.omit() %>%
+  setnames("Инфляция, % г/г","cpi_inflation")
+
+# Кредитный импульс
+credit_impl <- fread("3.Data/кредитный_импульс_помесячно.csv") %>%
+  as.data.table() %>%
+  .[,`Дата` := as.Date(paste0(`Дата`,"-01"), format = "%Y-%m-%d")]
+
+# Межбанковская реальная ставка
+st_ruonia <- read_xlsx("3.Data/RC_F11_01_2010_T04_07_2025.xlsx") %>%
+  as.data.table() %>%
+  .[,c("DT","ruo")] %>%
+  .[, ym := format(as.Date(DT), "%Y-%m")] %>%
+  .[, .(`ruonia_rate` = mean(ruo, na.rm = TRUE)), by = .(ym)] %>%
+  .[,`Дата` := as.Date(paste0(ym,"-01"), format = "%Y-%m-%d")] %>%
+  .[,-c("ym")]
+
+#ВВП
+gdp <- fread("3.Data/vvp_pomesyachno.csv") %>%
+  as.data.table() %>%
+  na.omit() %>%
+  setnames("ВВП_месяц","gdp_level")
+
+#Бюджетный импулс
+budget_impl <- read_xlsx("3.Data/budget.xlsx") %>%
+  as.data.table() %>%
+  setnames("Date","Дата") %>%
+  .[,`Дата` := as.Date(paste0(`Дата` ,"-01"), format = "%Y-%m-%d")] %>%
+  setnames("Value","Бюджетный импулс")
+
+#Скор санкций
+sunctions <- fread("3.Data/sunctions.csv") %>%
+  setnames(c("Дата","Скор санкций")) %>%
+  .[, ym := format(as.Date(`Дата`), "%Y-%m")] %>%
+  .[, .(`Скор_санкций` = mean(`Скор санкций`, na.rm = TRUE)), by = .(ym)] %>%
+  .[,`Дата` := as.Date(paste0(ym,"-01"), format = "%Y-%m-%d")] %>%
+  .[,-c("ym")]
+
+respectation_inf <- fread("3.Data/ожидаемая_инфляция.csv") %>%
+  na.omit() %>%
+  .[,`Дата` := as.Date(paste0(`Дата`,"-01"), format = "%Y-%m-%d")]
+
+#Валатильность активов
+wind <- 30
+all_input_data <- read_xlsx("3.Data/assets.xlsx") %>%
+  as.data.table() %>%
+  na.omit() %>%
+  .[order(`Дата`)] %>%
+  .[, all := MOEX + RTS] %>%
+  .[, vol := frollapply(all, wind, function(all){
+    mean <- mean(all)
+    res <- sum(abs(mean - all))/length(all)
+    })*sqrt(wind)] %>%
+  .[,c("Дата","vol")] %>%
+  na.omit() %>%
+  .[, ym := format(as.Date(`Дата`), "%Y-%m")] %>%
+  .[, .(vol = mean(vol, na.rm = TRUE)), by = .(ym)] %>%
+  .[,`Дата` := as.Date(paste0(ym,"-01"), format = "%Y-%m-%d")] %>%
+  .[,-c("ym")] %>%
+  merge(inflation, by = "Дата", all.x = TRUE) %>%
+  merge(credit_impl, by = "Дата", all.x = TRUE) %>%
+  merge(st_ruonia, by = "Дата", all.x = TRUE) %>%
+  merge(gdp, by = "Дата", all.x = TRUE) %>%
+  merge(budget_impl, by = "Дата", all.x = TRUE) %>%
+  # merge(sunctions, by = "Дата", all.x = TRUE) %>%
+  merge(respectation_inf, by = "Дата", all.x = TRUE) %>%
+  # .[1,`Скор_санкций`:=2037] %>%
+  .[, (names(.)[sapply(., is.numeric) & names(.) != "Дата"]) := lapply(.SD, function(x) {
+    if (any(is.na(x))) zoo::na.spline(x, na.rm = FALSE) else x}), .SDcols = names(.)[sapply(., is.numeric) & names(.) != "Дата"]] %>%
+  setnames("Дата", "date")
+
+
+
+prep_input_data <- copy(all_input_data) %>%
+  setorder(date) %>%
+  .[,gdp_trend := loess(gdp_level ~ as.numeric(date), span=0.3)$fitted] %>%
+  .[,gdp_gap_lag1  := lag(gdp_level, 1) - lag(gdp_trend, 1)] %>%
+  .[,cpi_infl_lag1 := lag(cpi_inflation, 1)] %>%
+  .[,gdp_gap2  := (gdp_level - gdp_trend) / gdp_trend * 100] %>%
+  .[,rate_diff := ruonia_rate - lag(ruonia_rate, 1)]
 
 # 2. Хвостовые распределения (оценка α‑stable, Poisson, Pareto) ####
-resid0 <- lm(ruonia_rate ~ cpi_inflation + gdp_gap2, data = dt) %>%
+resid0 <- lm(ruonia_rate ~ cpi_inflation + gdp_gap2, data = prep_input_data) %>%
   residuals()
 
-stable_fit <- resid0 %>% stable.fit()
+stable_fit <- resid0 %>% stable_fit()
 tail95     <- resid0[resid0 > quantile(resid0, .95, na.rm = TRUE)]
 pareto_fit <- fitdist(tail95, "pareto", start = list(shape = 1, scale = min(tail95, na.rm = TRUE)))
 lambda_hat <- mean(resid0 > quantile(resid0, .95, na.rm = TRUE))
