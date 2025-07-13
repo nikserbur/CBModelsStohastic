8f0cec96f5194283d08dd6d227593b91ff122fd4
nikserbur <quasiperelman@gmail.com>
2025-07-13 18:06:40 +0300

    ADD read me

--- 1.R/8.SourceAll.R
+++ 1.R/8.SourceAll.R
@@ -1,9 +1,18 @@
 # Централизованный соурс всех разработанных функций
-# Последний апдейт 09.04.2023
+# Стохастическое моделирование процентных ставок ЦБ
+# Последний апдейт 25.12.2024
 
+#### Подключение библиотек ####
 try(source("1.R/9.libraries.r"))
 
+#### Подключение всех функций проекта ####
+try(source("1.R/1.functions/HarVolatilityFunctions.R"))
+try(source("1.R/1.functions/HmmFunctions.R"))
+try(source("1.R/1.functions/BoostingFunctions.R"))
+try(source("1.R/1.functions/TalebR2Functions.R"))
+
+#### Настройки соединения ####
 # Чтобы не выдавал ошибку Error in curl::curl_fetch_memory(url, handle = handle) :SSL certificate problem: certificate has expired
 httr::set_config(httr::config(ssl_verifypeer = FALSE, ssl_verifyhost = FALSE))
 
-print("Sourced")
+cat("Все библиотеки и функции успешно подключены!\n")
